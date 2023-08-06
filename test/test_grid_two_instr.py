0259a47e2c7419077f8590392bf36ce1753b0bfc
kirrya95 <kirill.bogomolov95@gmail.com>
2023-08-06 14:27:04 +0300

    update_limit_order, upd_take_profit

--- test/test_grid_two_instr.py
+++ test/test_grid_two_instr.py
@@ -1,38 +1,69 @@
-from unittest import IsolatedAsyncioTestCase
-from unittest.mock import AsyncMock
-import unittest
-# from trading_bot.bot_two_instruments.grid_controller_two_instruments import GridControllerTwoInstruments
+import pytest
 import typing as tp
 import time
 import asyncio
 
-
 from utils import *
 
+# from trading_bot.bot_two_instruments.grid_controller_two_instruments import GridControllerTwoInstruments
 from trading_bot.bot_two_instruments.grid_two import GridControllerTwoInstruments
 
+from constants import GridDirections
 
 config = load_config('config.yaml')
 
 
-class TestGridControllerTwoInstruments(IsolatedAsyncioTestCase):
-    async def asyncSetUp(self):
-        self.controller = GridControllerTwoInstruments(ndigits_rounding=8)
-        self.controller.grid_step = 0.1  # Set a sample grid step
-
-    async def test_initialize_grid(self):
-        instr_price = 100.0
-        grid_size = 5
-        grid_direction = "up"
-
-        await self.controller.initialize_grid(instr_price, grid_size, grid_direction)
-
-        expected_levels = [100.1, 100.2, 100.3, 100.4, 100.5]
-        print(self.controller.grid)
-        for i, level_info in self.controller.grid.items():
-            self.assertEqual(level_info["level"], expected_levels[i])
-            self.assertFalse(level_info["reached"])
+@pytest.fixture
+def grid_controller():
+    controller = GridControllerTwoInstruments(ndigits_rounding=8)
+    controller.grid_step = 0.1  # Set a sample grid step
+    return controller
 
 
-if __name__ == '__main__':
-    IsolatedAsyncioTestCase.run_tests()
+@pytest.mark.asyncio
+async def test_initialize_grid(grid_controller):
+    spread_price = 100.0
+    grid_size = 5
+    grid_direction = GridDirections.GRID_DIRECTION_LONG
+
+    # grid = grid_controller
+    print('123')
+
+    grid_controller.initialize_grid(spread_price, grid_size, grid_direction)
+    print(grid_controller.grid)
+
+    expected_levels = [100.1, 100.2, 100.3, 100.4, 100.5]
+    for i, level_info in grid_controller.grid.items():
+        print(level_info.reached)
+        # assert level_info["level"] == expected_levels[i]
+        # assert not level_info["reached"]
+
+
+@pytest.mark.asyncio
+async def test_grid_overflow(grid_controller):
+    instr_price = 100.0
+    grid_size = 5
+    grid_direction = GridDirections.GRID_DIRECTION_LONG
+
+
+@pytest.mark.asyncio
+async def test_update_take_profit(grid_controller):
+    spread_price = 100.0
+    grid_size = 5
+    grid_direction = GridDirections.GRID_DIRECTION_LONG
+
+    # grid = grid_controller
+    print('123')
+
+    grid_controller.initialize_grid(spread_price, grid_size, grid_direction)
+    # print(grid_controller.grid)
+
+    order1_id = 1
+    order2_id = 2
+
+    grid_controller.update_limit_order(
+        level=99.7, order1_id=order1_id, order2_id=order2_id)
+
+    assert grid_controller.grid[99.7].limit_order_hash == hash(
+        (order1_id, order2_id))
+    print(grid_controller.grid[99.7])
--- trading_bot/bot_two_instruments/grid_two.py
+++ trading_bot/bot_two_instruments/grid_two.py
@@ -1,29 +1,98 @@
 from trading_bot.base_grid_controller import BaseGridController
+import typing as tp
 from copy import deepcopy
+from dataclasses import dataclass
 
 from utils import load_config, to_utc_timestamp
+from utils.error_checkers import check_grid_direction
+
 from constants import *
 
 config = load_config('config.yaml')
+
+
+@dataclass
+class GridEntry:
+    reached: bool
+    take_profit_level: int
+    limit_order_hash: tp.Union[str, None]
+    take_profit_order_hash: tp.Union[str, None]
 
 
 class GridControllerTwoInstruments(BaseGridController):
     def __init__(self, ndigits_rounding: int):
         super().__init__(ndigits_rounding=ndigits_rounding)
 
-        self.grid = {}
-        self.pending_limit_orders = {}
-        self.pending_take_profit_orders = {}
-        self.limit_to_take_profit_orders = {}  # limit -> take profit
-        self.take_profit_to_limit_orders = {}  # take profit -> limit
+        # TODO: change this
+        self.orders_in_market = 5
+        self.max_orders_amount = 10
+        self.grid: dict[int, GridEntry] = {}
+        # warning: this dict potentially can be very large
+        self.hash_to_orders = {}
+        # self.pending_limit_orders = {}
+        # self.pending_take_profit_orders = {}
+        # self.limit_to_take_profit_orders = {}  # limit -> take profit
+        # self.take_profit_to_limit_orders = {}  # take profit -> limit
 
-    async def initialize_grid(self, instr_price: float, grid_size: int, grid_direction: str):
-        ndigits_rounding = 8
+    @check_grid_direction
+    def initialize_grid(self, instr_price: float, grid_size: int, grid_direction: str):
         for i in range(grid_size):
-            level = round(instr_price + self.grid_step *
-                          (1+i), ndigits=ndigits_rounding)
-            self.grid[i] = {"level": level, "reached": False}
+            if grid_direction == GridDirections.GRID_DIRECTION_LONG:
+                level = round(instr_price - self.grid_step *
+                              (1+i), ndigits=self.grid_ndigits_rounding)
+                take_profit_level = level + self.grid_step
+            else:
+                level = round(instr_price + self.grid_step *
+                              (1+i), ndigits=self.grid_ndigits_rounding)
+                take_profit_level = level - self.grid_step
 
+            self.grid[level] = GridEntry(
+                take_profit_level=take_profit_level,
+                reached=False,
+                limit_order_hash=None,
+                take_profit_order_hash=None
+            )
 
+    def clear_level(self, level: float):
+        self.grid[level].reached = False
+        self.grid[level].limit_order_hash = None
+        self.grid[level].take_profit_order_hash = None
 
+    def update_limit_order(self, level: float, order1_id: int, order2_id: int):
+        # check that 'reached' attr is False, and limit order hash is None
+        self.check_limitOrder(level)
 
+        self.grid[level].limit_order_hash = self.calculate_hash(
+            order1_id, order2_id)
+        self.hash_to_orders[self.grid[level].limit_order_hash] = (
+            order1_id, order2_id)
+        self.grid[level].reached = True
+
+    def update_take_profit_order(self, level: float, order1_id: int, order2_id: int):
+        # check if level is reached and limit order hash is not None
+        self.check_takeProfitOrder(level)
+
+        take_profit_hash = self.calculate_hash(order1_id, order2_id)
+        self.grid[level].take_profit_order_hash = take_profit_hash
+        self.hash_to_orders[take_profit_hash] = (order1_id, order2_id)
+
+    @staticmethod
+    def calculate_hash(order1_id: int, order2_id: int):
+        return hash((order1_id, order2_id))
+
+    ### checkers ###
+
+    def check_limitOrder(self, level: float):
+        if self.grid[level].reached is True:
+            raise Exception(
+                f'Level {level} is already reached!')
+        if self.grid[level].limit_order_hash is not None:
+            raise Exception(
+                f'Limit order hash is not None for level {level}!')
+
+    def check_takeProfitOrder(self, level: float):
+        if self.grid[level].reached is False:
+            raise Exception(
+                f'Level {level} is not reached!')
+        if self.grid[level].limit_order_hash is None:
+            raise Exception(f'Limit order hash is None for level {level}!')
